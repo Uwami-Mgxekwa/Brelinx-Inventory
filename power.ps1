bccb9f93914b21b65dc003d8cfa4ce43fb8bf1f2
ZiloTech <uwamimcdonald@gmail.com>
2026-01-29 09:42:55 +0200

    fixes

--- power.ps1
+++
@@ -1,9 +0,0 @@
-cd "C:\Users\ZiloTech\Desktop\testingv1"
-for ($i=1; $i -le 600; $i++) {
-    $randstr = -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
-    Add-Content test.txt "Commit $i with random: $randstr"
-    git add test.txt
-    git commit -m "Commit $i: Added random string $randstr"
-    Write-Output "Commit $i done"
-}
-git push origin main
